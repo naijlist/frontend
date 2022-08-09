f04072d9b7c012dcd2f030101806d2c4696ad486
salissadiq <salissadiq@gmail.com>
2022-08-09 16:07:21 +0100

    Add ads-details view page-component

--- src/views/AdsDetails.vue
+++ src/views/AdsDetails.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        Product details
+    <div class="flex justify-around  gap-5 mx-20 my-28">
+        <div class="w-3/4 rounded-md">
+            <img src="../assets/imgs/lenovot450.jpg"  alt="ads-img" class="w-full rounded-md">
+        </div>
+        <div class="w-1/3 flex-col">
+            <div class=" bg-white  rounded-md px-5 py-5 mb-5">
+                <p class="text-4xl font-bold text-black">&#8358; 186,000</p>
+            </div>
+            <div class=" bg-white  rounded-md px-5 py-5 mb-5">
+                <div class="flex items-center gap-5">
+                    <img src="../assets/sals.jpg" class="rounded-md w-16" alt="">
+                    <div>
+                        <p class="font-bold text-xl">Salscode</p>
+                        <p class="px-2 text-md">Reply ASAP</p>
+                    </div>
+                </div>
+                <button class="bg-primary block px-10 py-3 mx-auto mt-5 w-full rounded-md text-white"><i class="fa fa-phone"></i> &nbsp; Show phone</button>
+            </div>
+            <div class=" bg-white  rounded-md px-5 py-5">
+                <img src="../assets/map.png" alt="">
+            </div>
+
+            
+        </div>
+        
     </div>
 </template>
 
@@ -13,3 +36,6 @@ export default {
     }
 }
 </script>
+<style scoped>
+
+</style>
