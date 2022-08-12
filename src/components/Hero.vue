247a14d050adf69c774e645d711039cba8c3b6ae
salissadiq <salissadiq@gmail.com>
2022-08-12 15:42:39 +0000

    Add ads-specification details in adsDetails page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -24,7 +24,9 @@
           focus:outline-none"
           type="text" 
           placeholder="Search for anything in Nigeria">
-          <svg xmlns="http://www.w3.org/2000/svg" @click="searchAds"  class="h-12 w-12 px-3 py-3 cursor-pointer flex items-center rounded-tr-md rounded-br-md  bg-black text-red hover:bg-gray-800 duration-500 transition ease-in " width="24" height="24" style="fill: white; user-select: auto;"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" style="user-select: auto;"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" @click="searchAds"  class="h-12 w-12 px-3 py-3 cursor-pointer flex items-center rounded-tr-md rounded-br-md  bg-black text-red hover:bg-gray-800 duration-500 transition ease-in " width="24" height="24" style="fill: white; user-select: auto;">
+            <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" style="user-select: auto;"></path>
+          </svg>
           
         </div>
       </div>
--- src/views/AdsDetails.vue
+++ src/views/AdsDetails.vue
@@ -3,21 +3,84 @@
         <div class="col-span-2 rounded-md">
             <img src="../assets/imgs/lenovot450.jpg"  alt="ads-img" class="w-full rounded-md mb-5">
         <div class=" col-span-2 rounded-md bg-white px-5 py-5"> 
-            <p class="text-3xl font-bold">Lenovo thinkPad 8GB RAM and 1TB HDD</p>
-            <div>
-                <p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); user-select: auto;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" style="user-select: auto;"></path><path d="M13 7h-2v6h6v-2h-4z" style="user-select: auto;"></path></svg>
+            <p class="text-md sm:text-3xl font-bold px-5">Lenovo thinkPad 8GB RAM and 1TB HDD</p>
+            <div class="px-3 sm:px-5 py-5 flex gap-5 sm:gap-10">
+                <p class="flex gap-2 justify-center items-center text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); user-select: auto;">
+                    <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" style="user-select: auto;"></path><path d="M13 7h-2v6h6v-2h-4z" style="user-select: auto;"></path>
+                </svg>
+                <span class="text-xs" >posted 1 hr ago</span>
                 </p>
-                <p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); user-select: auto;"><path d="M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z" style="user-select: auto;"></path><path d="M11.42 21.814a.998.998 0 0 0 1.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005.021 4.438-4.388 8.423-6 9.73-1.611-1.308-6.021-5.294-6-9.735 0-3.309 2.691-6 6-6z" style="user-select: auto;"></path></svg>
+                <p class="flex gap-2 justify-center items-center text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-700" width="24" height="24" style="fuser-select: auto;">
+                    <path d="M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z" style="user-select: auto;"></path><path d="M11.42 21.814a.998.998 0 0 0 1.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005.021 4.438-4.388 8.423-6 9.73-1.611-1.308-6.021-5.294-6-9.735 0-3.309 2.691-6 6-6z" style="user-select: auto;"></path>
+                    </svg>
+                    <span class="text-xs">Gwarinpa, Abuja</span>
+
                 </p>
+                <p class="flex gap-2 justify-center items-center text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); user-select: auto;">
+                        <path d="M12 9a3.02 3.02 0 0 0-3 3c0 1.642 1.358 3 3 3 1.641 0 3-1.358 3-3 0-1.641-1.359-3-3-3z" style="user-select: auto;"></path><path d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316-.105-.316C21.927 11.617 19.633 5 12 5zm0 12c-5.351 0-7.424-3.846-7.926-5C4.578 10.842 6.652 7 12 7c5.351 0 7.424 3.846 7.926 5-.504 1.158-2.578 5-7.926 5z" style="user-select: auto;"></path>
+                    </svg>
+                    <span class="text-xs sm:text-sm">100 views</span>
+                </p>
+            </div>
+            <hr/>
+            
+            <div class="px-5 py-5">
+                <h3 class="text-2xl  mb-4">Specification</h3>
+                <div class="flex  py-5 gap-5 sm:gap-10">
+                <p class="flex gap-2 justify-center items-cente text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" style="fill: rgb(0, 0, 0); user-select: auto;">
+                        <path d="M10 3H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM9 9H5V5h4v4zm11-6h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 6h-4V5h4v4zm-9 4H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1zm-1 6H5v-4h4v4zm8-6c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z" style="user-select: auto;"></path>
+                    </svg>
+                    <span class="text-xs"><b>Category:</b> Electronics</span>
+                </p>
+
+                <p class="flex gap-2 justify-center items-cente text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" style="fill: rgb(0, 0, 0); user-select: auto;">
+                        <path d="M10 3H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM9 9H5V5h4v4zm11-6h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 6h-4V5h4v4zm-9 4H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1zm-1 6H5v-4h4v4zm8-6c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z" style="user-select: auto;"></path>
+                    </svg>
+                    <span class="text-xs"><b>Condition:</b> Used</span>
+                </p>
+
+                 <p class="flex gap-2 justify-center items-cente text-gray-700">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" style="fill: rgb(0, 0, 0); transform: rotate(90deg); user-select: auto;">
+                    <path d="M11.707 2.293A.997.997 0 0 0 11 2H6a.997.997 0 0 0-.707.293l-3 3A.996.996 0 0 0 2 6v5c0 .266.105.52.293.707l10 10a.997.997 0 0 0 1.414 0l8-8a.999.999 0 0 0 0-1.414l-10-10zM13 19.586l-9-9V6.414L6.414 4h4.172l9 9L13 19.586z" style="user-select: auto;"></path><circle cx="8.353" cy="8.353" r="1.647" style="user-select: auto;"></circle>
+                   </svg>
+                   <span class="text-xs"> <b>Brand:</b> Lenovo</span>
+                </p>
+
+                 
+            </div>
+                <div class=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-0  sm:gap-40" >
+                    <div class="col-span-1">
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Model:</b> Lenovo t450</p>
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Storage capacity:</b> 1TB</p>
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>RAM:</b> 8GB</p>
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Processor:</b> intel core i5</p>
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Graphics card:</b> intel</p>
+                    </div>
+
+                    <div class="col-span-1">
+                        <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Operating system:</b> Windows 10 pro</p>
+                         <p class="bg-gray-200 px-4 py-2 my-5 rounded-md"><b>Color:</b> Black</p>
+                       
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="py-5 px-5">
+                <h3 class="text-2xl mb-5">Description</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolorum harum in veritatis ipsam, laborum nemo commodi reprehenderit voluptatibus quidem? Pariatur enim aut dignissimos vitae ratione quo qui eum nulla!</p>
             </div>
         </div>
 
         </div>
         <div class="col-span-2 md:col-span-1 flex-col">
-            <div class=" bg-white  rounded-md px-5 py-5 mb-5">
+            <div class="flex items-center gap-5 bg-white  rounded-md px-5 py-5 mb-5">
                 <p class="text-4xl font-bold text-black">&#8358; 186,000</p>
+                <span class="bg-gray-200 px-5 py-3 rounded-md font-semibold text-gray-700">Negotiable</span>
             </div>
             <div class=" bg-white  rounded-md px-5 py-5 mb-5">
                 <div class="flex items-center gap-5">
@@ -51,7 +114,8 @@
                 </button>
             </div>
             <div class=" bg-white  rounded-md px-5 py-5 mb-5">
-                <img src="../assets/map.png" alt="">
+                <!-- <img src="../assets/map.png" alt=""> -->
+                <object style="border:0; height: 230px; width: 100%;" data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d34015.943594576835!2d-106.43242624069771!3d31.677719472407432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75d90e99d597b%3A0x6cd3eb9a9fcd23f1!2sCourtyard+by+naijlist!5e0!3m2!1sen!2sbd!4v1533791187584"></object>
             </div>
 
         <div class="col-span-2 md:col-span-1 px-5 py-5 bg-white">NOTE: warning about naijlist</div>
