f19b729a6360d0fab71a0ad12c5847a250d24c4f
salissadiq <salissadiq@gmail.com>
2024-05-28 17:05:11 +0100

    Refactor Default layout to Main layout

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="bg-bgColor">
+    <AppNavber />
+    <div>
+      <router-view />
+    </div>
+    <AppFooter />
+  </main>
+</template>
+
+<script setup lang="ts">
+import AppNavber from '../components/Shared/AppNavber.vue'
+import AppFooter from '../components/Shared/AppFooter.vue'
+</script>
+
+<style scoped></style>
