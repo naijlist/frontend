28485bcd737a042710bb2faa2aea06a75aa09354
salissadiq <salissadiq@gmail.com>
2024-05-28 17:05:58 +0100

    Import category grid and ads grids

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HeroComponent from '../components/Home/HeroComponent.vue'
+import CategoriesGrid from '../components/Category/CategoriesGrid.vue'
+import AdsGrid from './../components/Ads/AdsGrid.vue'
 </script>
 
 <template>
   <main class="pt-[80px]">
     <HeroComponent />
+    <CategoriesGrid />
+    <AdsGrid />
   </main>
 </template>
