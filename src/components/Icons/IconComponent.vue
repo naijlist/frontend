87da2421f6a0003ebd62955b6696c3f726c87841
salissadiq <salissadiq@gmail.com>
2024-06-20 18:24:47 +0100

    Implement dynamic icon import accross the app

---
+++ src/components/Icons/IconComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-html="svgContent"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { icons } from './IconsItems'
+
+const props = defineProps({
+  icon: {
+    type: String
+  }
+})
+
+const svgContent = computed(() => {
+  return icons[props.icon] || ''
+})
+</script>
+
+<style scoped></style>
